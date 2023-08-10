d968e44e6e1faf7786c4e93c8bac6c0289b8a5ac
ezetap <santokalayil@gmail.com>
2023-08-10 22:53:41 +0530

    basic setup

---
+++ requirements/install.sh
@@ -0,0 +1,9 @@
+
+curl https://pyenv.run | bash
+# curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash # above link comes here first to install
+
+pyenv update
+
+
+
+
---
+++ requirements/sudo_install.sh
@@ -0,0 +1,7 @@
+# pyenv related things
+apt update -y
+apt install liblzma-dev libffi-dev libreadline-dev libsqlite3-dev tk -y
+
+# apt install python3-tk
+
+
