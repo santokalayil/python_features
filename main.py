825ab80429edd78a58af16fd26330176ec186a6f
ezetap <santokalayil@gmail.com>
2023-08-10 23:28:00 +0530

    relative sections added along with python version fetch

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+from lib.utils import find_python_version
+
+py_version = find_python_version()
+available_versions = ["3.8", "3.9", "3.10", "3.11"]
+if py_version in available_versions:
+    if py_version == "3.8":
+        from lib import v3_8 as v
+    elif py_version == "3.9":
+        from lib import v3_9 as v
+    elif py_version == "3.10":
+        from lib import v3_10 as v
+    elif py_version == "3.11":
+        from lib import v3_11 as v
+    elif py_version == "3.12":
+        from lib import v3_12 as v
+    else:
+        pass
+else:
+    raise Exception("The python version you are currently using is not supported")
+
---
+++ notebooks/prepare_nb.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+notebooks_dir = os.path.dirname(os.path.abspath(__file__))
+main_dir = os.path.dirname(notebooks_dir)
+
+sys.path.append(main_dir)
