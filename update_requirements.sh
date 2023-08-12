4f5af29b5f220d766bdc40a05277a154805d27e4
ezetap <santokalayil@gmail.com>
2023-08-12 12:13:59 +0530

    changes

--- update_requirements.sh
+++ update_requirements.sh
@@ -2,6 +2,7 @@ poetry export -f requirements.txt --output requirements/requirements.txt --witho
 poetry export --with dev -f requirements.txt --output requirements/requirements-dev.txt --without-hashes
 
 sed -i 's/;.*//' requirements/requirements-dev.txt
+# grep -E '^(autopep8|flake8|ipykernel)==.*' requirements/requirements-dev.txt > requirements/filtered-requirements-dev.txt
 sed -i 's/;.*//' requirements/requirements.txt
 
 
